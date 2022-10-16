2eacf0c4dfd513ef6900f703e0eebfa94d9b9098
LawyerN <68814524+LawyerN@users.noreply.github.com>
2022-10-16 11:33:45 +0200

    dropdony w sidebarze i skopiowanie slow do navu

--- js/app.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navigation").style.top = "0";
-  } else {
-    document.getElementById("navigation").style.top = "-50px";
-  }
-}
